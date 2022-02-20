70bf8f35c5af87d60f9c07416c718fb1879a4644
ForKio <55454285+TFerrarah@users.noreply.github.com>
2022-02-20 11:03:51 +0100

    Created stations (this is also a test commit.)

--- StationLy/src/App.java
+++ StationLy/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) {
-       InputUtils.enterToContinue();
+       //Inizialmente avevo pensato di creare la classe "Stazione"
+       //Station[6] stations = {new Station("Bari"), new Station("Foggia"), new Station("Pescara"), ...}
+
+       //Però la classe avrebbe avuto solo una proprietà: Il nome.
+
+       //Quindi ho fatto un semplice array di Stringhe
+
+       String[] stations = {"Bari", "Foggia", "Pescara", "Ancona", "Bologna", "Milano" };
+
+       for (String st : stations) {
+           System.out.println(st+"\n");
+       }
     }
 }
